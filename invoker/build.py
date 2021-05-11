b28e0fe16d8fe0ffc77a3f06c159fff4d8f04583
HM <hieumdd@gmail.com>
2021-05-11 17:29:13 +0700

    Updated main.yaml to new dir structure

--- invoker/build.py
+++ invoker/build.py
@@ -2,4 +2,4 @@ import os
 import json
 
 with open("invoker/tables.json", "w") as f:
-    json.dump({"tables": [i.split(".")[0] for i in os.listdir("schemas")]}, f)
+    json.dump({"tables": [i.split(".")[0] for i in os.listdir("config")]}, f)
