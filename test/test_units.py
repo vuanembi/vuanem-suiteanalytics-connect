ffab7e463edc39a1b2e5a9fbdb146c0bcc281be3
HM <hieumdd@gmail.com>
2021-05-14 17:53:37 +0700

    Added ACCOUNTS schema

--- test/test_units.py
+++ test/test_units.py
@@ -7,7 +7,7 @@ from .utils import assertion, encode_data
 
 
 def test_standard():
-    data = {"table": "LOCATIONS"}
+    data = {"table": "ACCOUNTS"}
     message = encode_data(data)
     req = Mock(get_json=Mock(return_value=message), args=message)
     res = main(req)
