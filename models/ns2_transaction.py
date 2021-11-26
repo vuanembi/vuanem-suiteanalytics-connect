d72dddd44768da338282f519074dd67fac16f723
HM <hieumdd@gmail.com>
2021-11-26 14:32:05 +0700

    feat: Added ns2_transaction

---
+++ models/ns2_transaction.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, String, DateTime
+
+from models.models import NetSuite
+from components import connector
+from components import getter
+from components import loader
+
+
+class ns2_transaction(NetSuite):
+    keys = {
+        "p_key": ["id"],
+        "rank_key": ["id"],
+        "incre_key": ["id"],
+        "rank_incre_key": ["lastmodifieddate"],
+        "row_num_incre_key": ["lastmodifieddate"],
+    }
+    query = """
+        SELECT
+            id,
+            lastmodifieddate,
+            recordtype
+        FROM
+            transaction
+        WHERE
+            lastmodifieddate >= TO_DATE('{{ start }}', 'YYYY-MM-DD HH24:MI:SS')
+            AND lastmodifieddate <= TO_DATE('{{ end }}', 'YYYY-MM-DD HH24:MI:SS')
+    """
+    schema = [
+        {"name": "id", "type": "INTEGER"},
+        {"name": "lastmodifieddate", "type": "TIMESTAMP"},
+        {"name": "recordtype", "type": "STRING"},
+    ]
+    columns = [
+        Column("code", String),
+        Column("datesent", DateTime(timezone=True)),
+        Column("externalid", String),
+        Column("id", Integer),
+        Column("promotion", Integer),
+    ]
+    connector = connector.NetSuite2Connector
+    getter = getter.IDIncrementalGetter
+    loader = [
+        # loader.PostgresIncrementalLoader,
+        loader.BigQueryIncrementalLoader,
+    ]
