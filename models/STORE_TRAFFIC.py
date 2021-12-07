ed3727c001b7a2858707fc1494d6cc8ec834ee0b
HM <hieumdd@gmail.com>
2021-12-07 12:23:39 +0700

    feat: Added more columns to STORE_TRAFFIC

--- models/STORE_TRAFFIC.py
+++ models/STORE_TRAFFIC.py
@@ -1,5 +1,3 @@
-from sqlalchemy import Column, Integer, String, DateTime
-
 from models.models import NetSuite
 from components import connector
 from components import getter
@@ -16,21 +14,34 @@ class STORE_TRAFFIC(NetSuite):
     }
     query = """
         SELECT
-            STORE_TRAFFIC.DATE_0,
-            STORE_TRAFFIC.DATE_CREATED,
-            STORE_TRAFFIC.GENDER_ID,
-            STORE_TRAFFIC.IS_INACTIVE,
-            STORE_TRAFFIC.LAST_MODIFIED_DATE,
-            STORE_TRAFFIC.LOCATION_ID,
-            STORE_TRAFFIC.STORE_TRAFFIC_ID,
-            STORE_TRAFFIC.SUBSIDIARY_ID,
-            STORE_TRAFFIC.TOTAL_TIMES_OF_VISITING,
-            STORE_TRAFFIC.TOTAL_VISITOR
+            DATE_0,
+            DATE_CREATED,
+            GENDER_ID,
+            IS_INACTIVE,
+            LAST_MODIFIED_DATE,
+            LOCATION_ID,
+            STORE_TRAFFIC_ID,
+            SUBSIDIARY_ID,
+            TOTAL_TIMES_OF_VISITING,
+            TOTAL_VISITOR,
+            AVERAGE_NO__OF_ITEMS_PER_SALE,
+            AVERAGE_VALUE_PER_SALES_ORDER,
+            CONVERTER_RATE,
+            EMPLOYEE_REFERENCE_ID,
+            NO__OF_SALES_ORDER,
+            NO__OF_SELLING_ITEMS,
+            PARENT_ID,
+            STORE_TRAFFIC_EXTID,
+            SUBSIDIARY_ID,
+            TIME_SLOT_ID,
+            TOTAL_SALES_ORDER_VALUE,
+            TRAFFIC_RATE,
+            TRAFFIC_SOURCES_ID
         FROM
             "Vua Nem Joint Stock Company".Administrator.STORE_TRAFFIC STORE_TRAFFIC
         WHERE
-            STORE_TRAFFIC.LAST_MODIFIED_DATE >= '{{ start }}'
-            AND STORE_TRAFFIC.LAST_MODIFIED_DATE <= '{{ end }}'
+            LAST_MODIFIED_DATE >= '{{ start }}'
+            AND LAST_MODIFIED_DATE <= '{{ end }}'
     """
     schema = [
         {"name": "DATE_0", "type": "TIMESTAMP"},
@@ -43,19 +54,20 @@ class STORE_TRAFFIC(NetSuite):
         {"name": "SUBSIDIARY_ID", "type": "INTEGER"},
         {"name": "TOTAL_TIMES_OF_VISITING", "type": "INTEGER"},
         {"name": "TOTAL_VISITOR", "type": "INTEGER"},
+        {"name": "AVERAGE_NO__OF_ITEMS_PER_SALE", "type": "INTEGER"},
+        {"name": "AVERAGE_VALUE_PER_SALES_ORDER", "type": "INTEGER"},
+        {"name": "CONVERTER_RATE", "type": "INTEGER"},
+        {"name": "EMPLOYEE_REFERENCE_ID", "type": "INTEGER"},
+        {"name": "NO__OF_SALES_ORDER", "type": "INTEGER"},
+        {"name": "NO__OF_SELLING_ITEMS", "type": "INTEGER"},
+        {"name": "PARENT_ID", "type": "INTEGER"},
+        {"name": "STORE_TRAFFIC_EXTID", "type": "STRING"},
+        {"name": "TIME_SLOT_ID", "type": "INTEGER"},
+        {"name": "TOTAL_SALES_ORDER_VALUE", "type": "INTEGER"},
+        {"name": "TRAFFIC_RATE", "type": "INTEGER"},
+        {"name": "TRAFFIC_SOURCES_ID", "type": "INTEGER"},
     ]
-    columns = [
-        Column("DATE_0", DateTime(timezone=True)),
-        Column("DATE_CREATED", DateTime(timezone=True)),
-        Column("GENDER_ID", Integer),
-        Column("IS_INACTIVE", String),
-        Column("LAST_MODIFIED_DATE", DateTime(timezone=True)),
-        Column("LOCATION_ID", Integer),
-        Column("STORE_TRAFFIC_ID", Integer),
-        Column("SUBSIDIARY_ID", Integer),
-        Column("TOTAL_TIMES_OF_VISITING", Integer),
-        Column("TOTAL_VISITOR", Integer),
-    ]
+    columns = []
     connector = connector.NetSuiteConnector
     getter = getter.TimeIncrementalGetter
     loader = [
