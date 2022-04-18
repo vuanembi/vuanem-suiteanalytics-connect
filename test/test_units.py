660392375b519eee215a6baf239a68e9100cc95b
HM <hieumdd@gmail.com>
2022-04-18 15:31:27 +0700

    ci: Update test_units

--- test/test_units.py
+++ test/test_units.py
@@ -1,16 +1,14 @@
-from unittest.mock import Mock
 from datetime import date, timedelta
 
 import pytest
 
-from main import main
-from models.models import TABLES
 from netsuite.pipeline import (
     static_pipelines,
     time_dynamic_pipelines,
     id_dynamic_pipelines,
 )
 from netsuite.netsuite_service import pipeline_service
+from tasks.tasks_service import TASKS, tasks_service
 
 TIME_RANGE = [
     # ("auto", (None, None)),
@@ -37,10 +35,14 @@ def parameterize(pipeline_group):
 
 
 class TestPipeline:
+    def service(self, pipeline, start, end):
+        res = pipeline_service(pipeline)(start, end)
+        print(res)
+        assert res["output_rows"] >= 0
+
     @pytest.mark.parametrize(**parameterize(static_pipelines))
     def test_static_pipeline(self, pipeline):
-        res = pipeline_service(pipeline)(None, None)
-        assert res["output_rows"] > 0
+        return self.service(pipeline, None, None)
 
     @pytest.mark.parametrize(**parameterize(time_dynamic_pipelines))
     @pytest.mark.parametrize(
@@ -49,11 +51,7 @@ class TestPipeline:
         ids=[tr[0] for tr in TIME_RANGE],
     )
     def test_time_dynamic_pipeline(self, pipeline, timerange):
-        res = pipeline_service(pipeline)(
-            timerange[0],
-            timerange[1],
-        )
-        assert res["output_rows"] >= 0
+        return self.service(pipeline, timerange[0], timerange[1])
 
     @pytest.mark.parametrize(**parameterize(id_dynamic_pipelines))
     @pytest.mark.parametrize(
@@ -62,90 +60,15 @@ class TestPipeline:
         ids=[ir[0] for ir in ID_RANGE],
     )
     def test_id_dynamic_pipeline(self, pipeline, idrange):
-        res = pipeline_service(pipeline)(
-            idrange[0],
-            idrange[1],
-        )
+        return self.service(pipeline, idrange[0], idrange[1])
 
 
-def process(data):
-    req = Mock(get_json=Mock(return_value=data), args=data)
-    res = main(req)
-    return res.get("results")
-
-
-class TestPipelines:
-    def assert_pipelines(self, res):
-        assert res["num_processed"] >= 0
-        if res["num_processed"] > 0:
-            for i in res["loads"]:
-                assert res["num_processed"] == i["output_rows"]
-
+class TestTasks:
     @pytest.mark.parametrize(
-        "table",
-        TABLES["standard"],
+        "task",
+        TASKS.keys(),
+        ids=TASKS.keys(),
     )
-    def test_standard(self, table):
-        data = {
-            "table": table,
-        }
-        res = process(data)
-        self.assert_pipelines(res)
-
-    @pytest.mark.parametrize(
-        "table",
-        [
-            *TABLES["time_incre"],
-            *TABLES["id_incre"],
-        ],
-    )
-    @pytest.mark.timeout(0)
-    def test_auto(self, table):
-        data = {
-            "table": table,
-        }
-        res = process(data)
-        self.assert_pipelines(res)
-
-    @pytest.mark.parametrize(
-        "table",
-        TABLES["time_incre"],
-    )
-    @pytest.mark.timeout(0)
-    def test_manual_time(self, table):
-        data = {
-            "table": table,
-            "start": TIME_START,
-            "end": TIME_END,
-        }
-        res = process(data)
-        self.assert_pipelines(res)
-
-    @pytest.mark.parametrize(
-        "table",
-        TABLES["id_incre"],
-    )
-    def test_manual_id(self, table):
-        data = {
-            "table": table,
-            "start": ID_START,
-            "end": ID_END,
-        }
-        res = process(data)
-        self.assert_pipelines(res)
-
-
-@pytest.mark.parametrize(
-    "mode",
-    [
-        "standard",
-        "incre",
-    ],
-)
-def test_tasks(mode):
-    res = process(
-        {
-            "mode": mode,
-        }
-    )
-    assert res["tasks"] > 0
+    def test_service(self, task):
+        res = tasks_service({"task": task})
+        res
